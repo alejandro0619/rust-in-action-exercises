c6b1d7e244d1387f572a9616f285a425a4c88c34
Alejandro <alejandrolpz0619@gmail.com>
2021-11-19 22:01:08 -0400

    slices vector
    files affected [1]

--- src/part 1/slices.rs
+++ src/part 1/slices.rs
@@ -1,2 +1,8 @@
 // Slices are dynamically sized array-like objects. The term dynamically sized means that their size is not known at compile time. 
 // Yet, like arrays, these don't expand or contract. The use of the word dynamic in dynamically sized is closer in meanings to dynamic typing.
+// Another important use for slices in their ability to act as a view on arrays. The term "view" here is taken from database technology
+// and means that slices can gain fast, read-only access to data without needing to copy anything around.
+
+// The problem with slices is that Rust wants to know the size of every object in your program, and slices are defined as not having a compile-time size
+// References to the rescue, As mentioned before in the discussion of dinamically sized, slices sizes is fixed in memory/ These are made up of two usize components, a pointer and a lenght, That's why you  typically see slices referred to in their referred form (&[T]) such as strings slices that takes the notation &str
+
---
+++ src/part 1/vector.rs
@@ -0,0 +1,52 @@
+// Vector (Vec<T>) are growable lists of T. Using vectors is extremely common in Rust code.
+// These incur a small runtime penalty compared to arrays because of the extra bookkeeping that must to be done to enable their size t change
+// their size over the time.
+
+// let's re implement an extended version of grep-line project.
+
+fn main(){
+  let ctx_lines = 2;
+  let needle = "oo";
+  let haystack = "\
+  Every face, every shop,
+  bedroom window, public-house, and
+  dark square is a picture
+  feverishly turned--in search of what?
+  It is the same with books.
+  What do we seek
+  throught millions of pages?";
+
+  let mut tags: Vec<usize> = vec![];  // holds line numbers where matches occur
+  let mut ctx: Vec<Vec<(usize, String)>> = vec![]; // contains a vector per match to hold the context lines.
+
+  for (i, line) in haystack.lines().enumerate() {
+    if line.contains(needle) {
+      tags.push(i);
+      let v = Vec::with_capacity(2*ctx_lines + 1);
+      ctx.push(v);
+    }
+  }
+  if tags.is_empty(){
+    return;
+  }
+  for (i, line) in haystack.lines().enumerate(){
+    for(j, tag) in tags.iter().enumerate() {
+      let lower_bound = tag.saturating_sub(ctx_lines);
+      let upper_bound = tag + ctx_lines;
+
+      if(i >=lower_bound) && (i<= upper_bound) {
+        let line_as_string = String::from(line);
+        let local_ctx = (i, line_as_string);
+        ctx[j].push(local_ctx);
+
+      }
+    }
+  }
+
+  for local_ctx in ctx.iter() {
+    for &(i, ref line) in local_ctx.iter(){
+      let line_num = i + 1;
+      println!("{} : {}", line_num, line);
+    }
+  }
+}
